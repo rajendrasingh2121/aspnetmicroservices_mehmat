6f774c5c6ac006fcfd573450b6008d6ae98c8f47
Rajendra singh <rajendra.singh2121@gmail.com>
2022-01-08 10:05:30 +0000

    Added discount grpc and service method

---
+++ src/Services/Discount/Discount.grpc/Mapper/DiscountProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Discount.grpc.Entities;
+using Discount.grpc.Protos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Discount.grpc.Mapper
+{
+    public class DiscountProfile : Profile
+    {
+        public DiscountProfile()
+        {
+            CreateMap<Coupon, CouponModel>().ReverseMap();
+        }
+    }
+}
