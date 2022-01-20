aa8afa1c591d2062ccf3c2ede024aa8348456efe
Rajendra singh <rajendra.singh2121@gmail.com>
2022-01-20 17:32:40 +0000

    order query

--- src/Services/Ordering/Ordering.Application/Mapping/MappingProfile.cs
+++ src/Services/Ordering/Ordering.Application/Mapping/MappingProfile.cs
@@ -1,4 +1,6 @@
 ﻿using AutoMapper;
+using Ordering.Application.Features.Orders.Queries.GetOrderList;
+using Ordering.Domain.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +11,11 @@ namespace Ordering.Application.Mapping
 {
    public class MappingProfile:Profile
     {
+        public MappingProfile()
+        {
+            CreateMap<Order, OrdersVm>().ReverseMap();
+            //CreateMap<Order, CheckoutOrderCommand>().ReverseMap();
+            //CreateMap<Order, UpdateOrderCommand>().ReverseMap();
+        }
     }
 }
